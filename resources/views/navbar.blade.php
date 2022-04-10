f5d07f68c5cb233072879fd7f06384370d81793f
lentrix <hawkmanlentrix@gmail.com>
2022-04-10 11:14:52 +0800

    update navbar name

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-success">
     <div class="container">
-      <a class="navbar-brand" href="#">{{env('APP_NAME')}}</a>
+      <a class="navbar-brand" href="{{url('/')}}">Simple Judging</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
